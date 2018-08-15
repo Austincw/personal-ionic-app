f584d2d7563fd3d5b078b8213aee23af2f47efb9
Austin Washington <awashington@austins-mbp.ncat.edu>
2018-08-15 15:36:11 -0400

    implementing email sendign service

--- api_controllers/api_controllers.py
+++ api_controllers/api_controllers.py
@@ -1,8 +1,17 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request,jsonify
+from email import email
 
 ionic_app = Blueprint('personal-ionic-app', __name__)
 
 
-@ionic_app.route('/email', methods=['GET', 'POST'])
+@ionic_app.route('/email', methods=['POST'])
 def sendContactEmail():
-    return "Hello, World!"
+
+    data = request.get_json(silent=True)
+
+    email_data = data['email']
+    message = data['msg']
+
+    email.sendEmail(email_data, message)
+
+    return
---
+++ email/email.py
@@ -0,0 +1,35 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import os
+
+
+def sendEmail(email, bodyInformation):
+    try:
+        cc = [os.environ['EMAIL']]
+
+        emailServer = smtplib.SMTP('smtp.gmail.com', 587)
+        emailServer.starttls()
+        emailServer.login(os.environ['EMAIL'], os.environ['EMAIL_PASSWORD'])
+
+        message = MIMEMultipart()
+
+        message['From'] = os.environ['EMAIL']
+        message['To'] = email
+        message['CC'] = ",".join(cc)
+        cc.append(email)
+        message['Subject'] = "I'll be in touch soon"
+
+        body = bodyInformation
+        message.attach(MIMEText(body, 'plain'))
+
+        emailServer.sendmail(os.environ['EMAIL'], cc, message.as_string())
+        emailServer.quit()
+
+    except KeyError as e:
+
+        print(e + 'Missing Email or Password, no email was sent')
+
+
+    return
+
